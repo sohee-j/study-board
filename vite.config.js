574372c3cbb566292a6c68ea43271c9b7d40971f
sohee-j <sohee0402@naver.com>
2024-01-15 17:26:55 +0900

    router history 수정

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
 	plugins: [vue()],
-	base: import.meta.NODE_ENV === 'production' ? '/study-board/' : '',
+	base: import.meta.NODE_ENV === 'production' ? '/study-board/' : './',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
