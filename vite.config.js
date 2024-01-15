c628fabe75c0b27a29cbcc788ed6f1574c9cc234
sohee-j <sohee0402@naver.com>
2024-01-15 16:43:07 +0900

    env 설정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: './',
+	base: import.meta.NODE_ENV === 'production' ? '/study-board/' : '',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
