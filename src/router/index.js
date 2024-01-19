ac10c18e072f80a45694ebda622d381f2598eeea
sohee-j <sohee0402@naver.com>
2024-01-19 10:36:36 +0900

    router hash모드로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-	history: createWebHistory('/study-board/'),
+	history: createWebHashHistory('/study-board/'),
 	routes: [
 		{
 			path: '/',
